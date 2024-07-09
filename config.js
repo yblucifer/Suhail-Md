b20a20e189042096893b104b8659239d9fac1f5a
yblucifer <155779897+yblucifer@users.noreply.github.com>
2024-07-09 15:23:42 +0100

    config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ global.email ="saimsamsun789@gmail.com"
 global.location="Lahore,Pakistan."
 
 
-global.mongodb= process.env.MONGODB_URI || ""
+global.mongodb= process.env.MONGODB_URI || "mongodb+srv://Ukadik:09049001826s@cluster0.kjiojco.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 global.allowJids= process.env.ALLOW_JID || "null" 
 global.blockJids= process.env.BLOCK_JID || "null"
 global.DATABASE_URL = process.env.DATABASE_URL || ""
